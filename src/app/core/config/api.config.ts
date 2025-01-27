90098597d82cbffdd589aa6e3f41f4d60f4fd670
Mitakshar Hegde <work.mitakshar@gmail.com>
2025-01-27 11:20:12 +0530

    Update api.config.ts

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
  // BASE_URL: 'http://localhost:5454/v1',
-  BASE_URL:'https://ecom-backend-api.railway.internal/v1',
+  BASE_URL:'http://ecom-backend-api.railway.internal/v1',
   AUTH_TOKEN_KEY: 'auth_token',
   ENDPOINTS: {
     AUTH: {
